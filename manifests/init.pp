3816f0499e8afc3a444a3efe879ae61fc1ac958c
Alexander Tsirel <noma4i@gmail.com>
2015-02-02 16:01:17 +1100

    --wip--

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,22 @@
-class windows_applocker (
+define windows_applocker (
   $ensure   = 'enabled'
 ){
   case $ensure {
     'enabled', 'present': {
-
+      $permanent_id = 100
+      $identity = 'Everyone'
+      $app_path = 'C:\Program Files\Windows Mail\wabmig.exe'
+      $app_name = 'wabmig.exe'
+      $action = 'Allow'
+      exec { "Allow ${app_name}":
+        command  => template('windows_applocker/rule_hash.ps1.erb'),
+        provider => 'powershell',
+        timeout  => 1800
+      }->
+      exec { 'cmd /c gpupdate || exit /b 0':
+        path => $::path,
+        logoutput => false
+      }
     }
     default: {
       fail('Invalid ensure option!\n')
